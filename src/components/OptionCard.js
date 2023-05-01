ebe1c2f66623860204f262d5427c31fa697f8888
Yevhen Volkov <eugenewolf507@gmail.com>
2023-05-01 23:34:15 +0300

    add user photos

--- src/components/OptionCard.js
+++ src/components/OptionCard.js
@@ -1,10 +1,11 @@
-const OptionCard = ({ id, author, recipient, about }) => (
+const OptionCard = ({ id, author, recipient, about, handleChooseOption }) => (
   <div>
     <p>{id}</p>
     <p>
       {author} для {recipient}
     </p>
     <p>{about}</p>
+    <button onClick={() => handleChooseOption(id)}>Віддати голос</button>
   </div>
 );
 
