9e4c08539deb1dc52615eb894f765ff41d9825f4
Yevhen Volkov <eugenewolf507@gmail.com>
2023-05-02 22:15:03 +0300

    add styles for card

--- src/components/OptionCard.js
+++ src/components/OptionCard.js
@@ -9,26 +9,30 @@ const OptionCard = ({
   youtubeID,
   handleChooseOption,
 }) => (
-  <div>
-    <Image
-      src={`/usersPhoto/${userPhoto}.jpeg`}
-      alt="Picture of the author"
-      width={437}
-      height={437}
-      className="user-image"
-    />
-    <p className="sub-title">
-      {author} для {recipient}
-    </p>
-    <iframe
-      // width="420"
-      width="620"
-      height="345"
-      src={`https://www.youtube.com/embed/${youtubeID}`}
-    ></iframe>
-
-    <p>{about}</p>
-    <button onClick={() => handleChooseOption(id)}>Віддати голос</button>
+  <div className="card">
+    <div className="card__header">
+      <Image
+        src={`/usersPhoto/${userPhoto}.jpeg`}
+        alt="Picture of the author"
+        width={437}
+        height={437}
+        className="user-image"
+      />
+      <p className="card__title">
+        {author} для {recipient}
+      </p>
+    </div>
+    <div className="card__main">
+      <iframe
+        // width="430"
+        // height="250"
+        src={`https://www.youtube.com/embed/${youtubeID}`}
+      ></iframe>
+      <p className="card__about">{about}</p>
+    </div>
+    <button onClick={() => handleChooseOption(id)} className="btn">
+      Віддати голос
+    </button>
   </div>
 );
 
