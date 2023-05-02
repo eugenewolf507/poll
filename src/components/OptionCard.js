1d8b69a5900fd406428ea38e1d1810656c1360fc
Yevhen Volkov <eugenewolf507@gmail.com>
2023-05-02 15:35:35 +0300

    add videos from youtube

--- src/components/OptionCard.js
+++ src/components/OptionCard.js
@@ -1,9 +1,32 @@
-const OptionCard = ({ id, author, recipient, about, handleChooseOption }) => (
+import Image from 'next/image';
+
+const OptionCard = ({
+  id,
+  author,
+  recipient,
+  about,
+  userPhoto,
+  youtubeID,
+  handleChooseOption,
+}) => (
   <div>
-    <p>{id}</p>
-    <p>
+    <Image
+      src={`/usersPhoto/${userPhoto}.jpeg`}
+      alt="Picture of the author"
+      width={437}
+      height={437}
+      className="user-image"
+    />
+    <p className="sub-title">
       {author} для {recipient}
     </p>
+    <iframe
+      // width="420"
+      width="620"
+      height="345"
+      src={`https://www.youtube.com/embed/${youtubeID}`}
+    ></iframe>
+
     <p>{about}</p>
     <button onClick={() => handleChooseOption(id)}>Віддати голос</button>
   </div>
--- src/components/PollOptions.js
+++ src/components/PollOptions.js
@@ -5,16 +5,20 @@ const PollOptions = ({ handleChooseOption }) => {
   console.log(financialHelpData);
   return (
     <div>
-      {financialHelpData.map(({ id, author, recipient, about }) => (
-        <OptionCard
-          key={id}
-          id={id}
-          author={author}
-          recipient={recipient}
-          about={about}
-          handleChooseOption={handleChooseOption}
-        />
-      ))}
+      {financialHelpData.map(
+        ({ id, author, recipient, about, userPhoto, youtubeID }) => (
+          <OptionCard
+            key={id}
+            id={id}
+            author={author}
+            recipient={recipient}
+            about={about}
+            userPhoto={userPhoto}
+            youtubeID={youtubeID}
+            handleChooseOption={handleChooseOption}
+          />
+        )
+      )}
     </div>
   );
 };
